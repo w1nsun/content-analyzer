7739673a726d76eb3f4fd1bffb5b17c74a02bddf
WinSun <ilya.nevmyvaka@gmail.com>
2015-07-18 14:42:44 +0300

    remove config files

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=myproject',
-    'username' => 'dbuser',
-    'password' => 'dbuser',
-    'charset' => 'utf8',
-];
--- config/local.php
+++
@@ -1,5 +0,0 @@
-<?php
-
- return [
-     'cookieValidationKey' => 'K5bsQNhkV4Io3C56Q-X3wVJq_E0YvR8x',
- ];
