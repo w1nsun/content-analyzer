27c4a76fdf30c9be180108c6aed2834c74c779c4
WinSun <ilya.nevmyvaka@gmail.com>
2015-07-28 12:10:10 +0300

    add templates config files

---
+++ config/~local.php
@@ -0,0 +1,12 @@
+<?php
+
+ return [
+     'cookieValidationKey' => 'cookie_validation_key',
+     'db' => [
+         'class' => 'yii\db\Connection',
+         'dsn' => 'mysql:host=localhost;dbname=db_name',
+         'username' => 'username',
+         'password' => 'password',
+         'charset' => 'utf8',
+     ]
+ ];
