35165d8d539a2533503f90657b09c0e3408893f1
WinSun <ilya.nevmyvaka@gmail.com>
2015-11-13 18:19:35 +0200

    fix

---
+++ components/ArticleScore.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\components;
+
+use yii\base\Component;
+
+//https://gist.github.com/jonathanmoore/2640302
+class ArticleScore extends Component
+{
+    const SOCIAL_FACEBOOK    = 'facebook';
+    const SOCIAL_TWITTER     = 'twitter';
+    const SOCIAL_PINTEREST   = 'pinterest';
+    const SOCIAL_LINKEDIN    = 'linkedin';
+    const SOCIAL_GOOGLE_PLUS = 'google-plus';
+    const SOCIAL_VKONTAKTE   = 'vkontakte'; //https://vk.com/share.php?act=count&index=1&url=http://promorepublic.com/&format=json&callback=?
+
+
+}
