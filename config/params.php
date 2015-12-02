7084f8f525ec733c651359f1781789a5220dde63
w1nsun <ilya.nevmyvaka@gmail.com>
2015-12-02 12:51:51 +0200

    cleanup

--- config/params.php
+++ config/params.php
@@ -3,11 +3,4 @@
 return [
     'projectName' => 'ETrends',
     'adminEmail'  => 'admin@example.com',
-    'images'      => [
-        'article' => [
-            'prefix'        => 'article_',
-            'allowed_types' => [IMAGETYPE_BMP, IMAGETYPE_JPEG, IMAGETYPE_PNG],
-            'sizes'         => []
-        ]
-    ]
 ];
